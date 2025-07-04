84003073fe7cfa6b5159a72d7b6cc21e40d2b74f
Anaghaprasad123 <anaghaprasadmg@gmail.com>
2025-07-04 06:56:03 +0530

    Python programes

---
+++ 1.py
@@ -0,0 +1,5 @@
+name1="Anagha"
+name2="Anagha Prasad"
+print("My name is",name1)
+print("My name is",name2)
+
---
+++ 4.py
@@ -0,0 +1,4 @@
+def add(nums):
+    return nums[0]+nums[1]
+list1=[12,15]
+print(add(list1))
